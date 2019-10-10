198c6bd6d9a92e6309ab3092b6e67466ef2e0cf6
Jaime <jaime@d1tw.org>
2019-10-10 16:02:08 -0700

    Working on agent code. Nothing to show for it yet in terms of demonstration, but for reference, here's the API I plan on probably using.

---
+++ New Unity Project/Assets/Scripts/RunnerBehavior.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerBehavior : MonoBehaviour
+{
+    private RunnerManagerBehavior manager = null;
+    private int runnerID = -1;
+    private int destinationTrack = -1;
+    private bool alive = true;
+    private float height = 1.0f;
+    private bool dashing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider2D coll = gameObject.GetComponent<Collider2D>();
+        if (coll != null)
+        {
+            height = coll.bounds.size.y;
+        }
+    }
+
+    /*  --GrantRunnerID--
+        Called by RunnerManagerBehavior to form a parent connection
+        back to the runner manager. Don't manually call this later
+        on a different RunnerManagerBehavior, or else everything
+        will go belly up. */
+    public void GrantRunnerID(int id, RunnerManagerBehavior man)
+    {
+        manager = man;
+        runnerID = id;
+        destinationTrack = id;
+    }
+
+    /*  --HaveRunnerID--
+        Indicates whether this instance has been assigned a runner
+        ID by a runner manager. If not, we can't really do anything
+        useful as a runner. */
+    public bool HaveRunnerID()
+    {
+        return manager != null && runnerID >= 0;
+    }
+
+    /*  --RunnerID--
+        Runner ID getter. */
+    public int RunnerID()
+    {
+        return runnerID;
+    }
+
+    /*  --Alive--
+        A runner is considered alive if it exists, has been assigned
+        a runner ID, and either hasn't died, or hasn't died since
+        it was last resurrected. If you're not alive, you're not
+        in the game for now. */
+    public bool Alive()
+    {
+        return gameObject.activeSelf && HaveRunnerID() && alive;
+    }
+
+    /*  --Kill, Resurrect--
+        Alive setters. */
+    public void Kill()
+    {
+        alive = false;
+    }
+    public void Resurrect()
+    {
+        alive = true;
+    }
+
+    /*  --Track--
+        Returns the track the runner is on. This isn't just a getter
+        for 'destinationTrack'; for game logic purposes, a runner
+        is considered to be "on" a track if they're physically
+        within its region. */
+    public int Track()
+    {
+        float raw = gameObject.transform.position.y
+            / manager.TrackHeight() - 0.5f;
+        int rounded = (int) Math.Round(raw);
+        return manager.ConstrainID(rounded);
+    }
+
+    /*  --DestTrack--
+        Getter for destinationTrack. */
+    public int DestTrack()
+    {
+        return destinationTrack;
+    }
+
+    /*  --ChangingTracks--
+        Returns whether the runner is changing tracks. This is
+        determined by checking if the track the runner is on
+        equals the track the runner wants to be on. */
+    public bool ChangingTracks()
+    {
+        return Track() != destinationTrack;
+    }
+
+    /*  --OnTrack--
+        Returns whether the runner is on the given track. A runner
+        can technically be on more than one track at once, namely
+        while transitioning between them, in which case OnTrack
+        will return true for all tracks the runner is on, but Track
+        will only return the track the runner is "most" on.
+        Note, however, that unless the runner is changing tracks,
+        the runner can only be considered to be on the track
+        they've already changed to. So this rule about being
+        on multiple tracks only applies when they are in fact
+        changing tracks. */
+    public bool OnTrack(int track)
+    {
+        if (!ChangingTracks())
+        {
+            return track == destinationTrack;
+        }
+        else
+        {
+            float top = gameObject.transform.position.y
+                - height/2;
+            float bottom = gameObject.transform.position.y
+                + height/2;
+            float trtop = manager.TrackTop(track);
+            float trbottom = manager.TrackBottom(track);
+            return (top < trtop && bottom > trtop)
+                || (top < trbottom && bottom > trbottom)
+                || (top > trtop && bottom < trbottom);
+        }
+    }
+
+    /*  --MoveUpTrack, MoveDownTrack--
+        Switches tracks. Using these functions, you can only switch
+        to a track adjacent to the one you currently occupy
+        or cancel such a switch already in progress. */
+    public void MoveUpTrack()
+    {
+        int track = Track();
+        if (track < destinationTrack)
+        {
+            destinationTrack = track;
+        }
+        else if (track == destinationTrack)
+        {
+            destinationTrack =
+                manager.ConstrainID(destinationTrack - 1);
+        }
+    }
+    public void MoveDownTrack()
+    {
+        int track = Track();
+        if (track > destinationTrack)
+        {
+            destinationTrack = track;
+        }
+        else if (track == destinationTrack)
+        {
+            destinationTrack =
+                manager.ConstrainID(destinationTrack + 1);
+        }
+    }
+
+    /*  --ForceMoveTrack--
+        Switches tracks while ignoring the rules outlined above. */
+    public void ForceMoveTrack(int track)
+    {
+        destinationTrack = track;
+    }
+
+    /*  --Dashing--
+        Returns whether the runner is dashing. Dashing is a possible
+        player move that allows players to temporarily move farther
+        ahead on the track. */
+    public bool Dashing()
+    {
+        return dashing;
+    }
+
+    /*  --BeginDash, EndDash--
+        Dash setters. */
+    public void BeginDash()
+    {
+        dashing = true;
+    }
+    public void EndDash()
+    {
+        dashing = false;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RunnerManagerBehavior.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerManagerBehavior : MonoBehaviour
+{
+    /*  --runners, runnerBehaviors--
+        Note that runners' indices in these arrays
+        are defined as their "runner ID." Most functions
+        in the rest of this class provide data about runners
+        by their runner ID, not by reference. */
+    public GameObject[] runners;
+    private RunnerBehavior[] runnerBehaviors;
+    private float neutralPosition;
+    private float dashPosition;
+
+    /*  --PlayAreaHeight, PlayAreaWidth--
+        Gets the dimensions of the game world.
+        Thanks to dakotapearl on answers.unity.com
+        for the implementations. */
+    public float PlayAreaHeight()
+    {
+        return Camera.main.orthographicSize*2.0f;
+    }
+    public float PlayAreaWidth()
+    {
+        return PlayAreaHeight()*Camera.main.aspect;
+    }
+
+    /*  --ConstrainID--
+        Clamps the given integer to the range of valid
+        runner and track IDs. */
+    public int ConstrainID(int id)
+    {
+        if (id < 0)
+        {
+            return 0;
+        }
+        if (id >= runners.Length)
+        {
+            return runners.Length - 1;
+        }
+        return id;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        neutralPosition = PlayAreaWidth()/4;
+        dashPosition = 3*PlayAreaWidth()/8;
+        runnerBehaviors = new RunnerBehavior[runners.Length];
+        for (int i = 0; i < runners.Length; ++i)
+        {
+            if (runners[i] != null)
+            {
+                runnerBehaviors[i] =
+                    runners[i].GetComponent<RunnerBehavior>();
+                if (runnerBehaviors[i] == null)
+                {
+                    Console.Error.WriteLine(
+                        "WARN: Cannot register non-runner " +
+                        "GameObject with runner manager " +
+                        "at player index {0}.",
+                    i);
+                    runners[i] = null;
+                }
+                else
+                {
+                    runnerBehaviors[i].GrantRunnerID(i, this);
+                }
+            }
+        }
+    }
+
+    /*  --Alive--
+        Returns the list of runners still alive. */
+    public List<int> Alive()
+    {
+        List<int> ret = new List<int>();
+        foreach (RunnerBehavior rb in runnerBehaviors)
+        {
+            if (rb != null && rb.Alive())
+            {
+                ret.Add(rb.RunnerID());
+            }
+        }
+        return ret;
+    }
+
+    /*  --OnTrack--
+        Returns the list of runners on any given track.
+        There are as many tracks as there are runners.
+        Each runner starts on the track whose ordinal from the top
+        matches their runner ID. When a runner with a given
+        runner ID dies, the respective matching track
+        is disabled. */
+    public List<int> OnTrack(int track)
+    {
+        track = ConstrainID(track);
+        List<int> ret = new List<int>();
+        foreach (int i in Alive())
+        {
+            if (runnerBehaviors[i].OnTrack(track))
+            {
+                ret.Add(runnerBehaviors[i].RunnerID());
+            }
+        }
+        return ret;
+    }
+
+    /*  --TrackHeight--
+        Returns the height of the tracks. */
+    public float TrackHeight()
+    {
+        return PlayAreaHeight()/runners.Length;
+    }
+
+    /*  --TrackTop, TrackCenter, TrackBottom--
+        Returns the Y coordinates of the respective parts
+        of the given track. */
+    public float TrackTop(int track)
+    {
+        track = ConstrainID(track);
+        return TrackHeight()*track;
+    }
+    public float TrackCenter(int track)
+    {
+        track = ConstrainID(track);
+        return TrackTop(track) + TrackHeight()/2;
+    }
+    public float TrackBottom(int track)
+    {
+        track = ConstrainID(track);
+        return TrackTop(track) + TrackHeight();
+    }
+
+    /*  --NeutralX, NeutralY--
+        Given a runner ID, returns the coordinates of the point
+        the runner should generally occupy. Calculates these
+        coordinates based on the runner's current track, and,
+        if applicable, other runners on that track. */
+    public float NeutralX(int who)
+    {
+        who = ConstrainID(who);
+        RunnerBehavior rb = runnerBehaviors[who];
+        if (rb == null || !rb.Alive())
+        {
+            return 0.0f;
+        }
+        else if (rb.Dashing())
+        {
+            return dashPosition;
+        }
+        else
+        {
+            return neutralPosition;
+        }
+    }
+    public float NeutralY(int who)
+    {
+        who = ConstrainID(who);
+        RunnerBehavior rb = runnerBehaviors[who];
+        if (rb == null || !rb.Alive())
+        {
+            GameObject go = runners[who];
+            if (go == null)
+            {
+                return PlayAreaHeight()/2;
+            }
+            else
+            {
+                return go.transform.position.y;
+            }
+        }
+        else
+        {
+            int track = ConstrainID(rb.Track());
+            List<int> neighbors = OnTrack(track);
+            int ordinal = -1;
+            for (int i = 0; i< neighbors.Count; ++i)
+            {
+                if (neighbors[i] == who)
+                {
+                    ordinal = i;
+                    break;
+                }
+            }
+            if (ordinal < 0)
+            {
+                return TrackCenter(track);
+            }
+            else
+            {
+                return TrackTop(track) +
+                    (ordinal + 1)*TrackHeight()
+                    / (neighbors.Count + 1);
+            }
+        }
+    }
+}
