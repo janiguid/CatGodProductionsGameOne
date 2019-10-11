9d1f14eb04cdd05614a75a897a8f42b7b942c3db
Jessie Dulay Aniguid <janiguid@crm.ucsc.edu>
2019-10-11 16:51:09 -0700

    Update Inventory system so it works with different inputs from different players

---
+++ New Unity Project/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    /*
+     * Controls as of right now:
+     * P1: shift is q, left is o and right is p
+     * P2: shift is a, left is k and right is l
+     * P3: shift is z, left is n and right is m
+     */
+
+    public RunnerInventory[] Inventories;
+
+    private static readonly string[][] controls =
+    {
+        new string[]
+        {
+            "P1 Shift",
+            "P1 Switch Left",
+            "P1 Switch Right",
+            "P1 Use Item"
+        },
+        new string[]
+        {
+            "P2 Shift",
+            "P2 Switch Left",
+            "P2 Switch Right",
+            "P2 Use Item"
+        },
+        new string[]
+        {
+            "P3 Shift",
+            "P3 Switch Left",
+            "P3 Switch Right",
+            "P3 Use Item"
+        },
+        new string[]
+        {
+            "P4 Shift",
+            "P4 Switch Left",
+            "P4 Switch Right",
+            "P4 Use Item"
+        }
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Goes through an array of RunnerInventory 
+    // and goes through each one, checking each 
+    // player's input
+    void Update()
+    {
+        for(int i = 0; i < Inventories.Length; ++i)
+        {
+            Inventories[i].UpdateInput(
+                Input.GetButton(controls[i][0]),
+                Input.GetButtonDown(controls[i][1]),
+                Input.GetButtonDown(controls[i][2]),
+                Input.GetButtonDown(controls[i][3]));
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RunnerInventory.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerInventory : MonoBehaviour
+{
+    [SerializeField]
+    private int InventorySize = 3;
+
+    public GameObject [] ItemList;
+    public InventoryData MyData;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemList = MyData.ItemList;
+    }
+
+    // Called in InventoryManager
+    // and uses data passed onto it to either:
+    // Use an item, switch right, or switch left
+    public void UpdateInput(bool Swapping, bool Left, bool Right, bool Use)
+    {
+        if (Use)
+        {
+            UseItem();
+        }
+        else if (Swapping && Right)
+        {
+            SwitchRight();
+        }
+        else if (Swapping && Left)
+        {
+            SwitchLeft();
+        }
+    }
+
+    //Need to set array spot to null
+    void UseItem()
+    {
+        //Instantiate game object
+    }
+
+    //Takes inventory array and shift its 
+    //elements to the right
+    void SwitchRight()
+    {
+        print("switched to the right");
+
+        GameObject temp = ItemList[2];
+        ItemList[2] = ItemList[1];
+        ItemList[1] = ItemList[0];
+        ItemList[0] = temp;
+
+    }
+
+    //Takes inventory array and shift its 
+    //elements to the right
+    void SwitchLeft()
+    {
+        print("switched to the left");
+
+        GameObject temp = ItemList[0];
+        ItemList[0] = ItemList[1];
+        ItemList[1] = ItemList[2];
+        ItemList[2] = temp;
+
+    }
+
+
+
+}
