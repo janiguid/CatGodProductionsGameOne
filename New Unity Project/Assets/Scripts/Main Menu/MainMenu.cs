499d0b5c07f846d2c4c017e4dc39243b347fca4b
Jessie Aniguid <jessieaniguid@gmail.com>
2019-10-17 10:46:56 -0700

    Add Transition from Main Menu to Character Selection Scene

---
+++ New Unity Project/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void BeginPlay()
+    {
+        SceneManager.LoadScene("Character Selection");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
