0d6a60cdf39ec51f68f4aa64b514617f2196ad0c
marsteve <marsteve@ucsc.edu>
2019-10-17 19:13:20 -0700

    need an endgame screen, have the boulder/spikes/finish line need runner script to run my destroy track script when runners die

---
+++ New Unity Project/Assets/FinishLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D pc) {
+    //for the types that slow all players, will need to access all the players somehow
+        if (pc.CompareTag("Player")) {
+            //need some kinda logic to get to the endgame screen with a victory
+            //scenes not built yet tho lol
+            //probably want some kinda in between animation that changes scene on animation end
+            //anyways use this
+            //SceneManager.LoadScene("EndGame");
+        }
+    }
+}
--- New Unity Project/Assets/Scripts/BoulderObstacle.cs
+++ New Unity Project/Assets/Scripts/BoulderObstacle.cs
@@ -2,29 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnablesController : MonoBehaviour {
-    
-    public float speed = 1;
-    public int spawnpointCount = 3;
-    public bool jumpable = true;
+public class BoulderObstacle : MonoBehaviour {
     public float knockbackDistance = 1f;
-    //speed will be modified by the obstacle controller too. 
-    void Update() {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-    }
-    void OnBecameInvisible() {
-        Destroy(gameObject);
-    }
-    void killObstacle() {
-        //wanna have like a particle emitter effect maybe?
-    }
+
     void OnTriggerEnter2D(Collider2D pc) {
     //for the types that slow all players, will need to access all the players somehow
         if (pc.CompareTag("Player")) {
             // if the obstacle knocks back only one player
             // need the name of the script for this to work right
             pc.GetComponent<RunnerBehavior>().Knockback(knockbackDistance);
-            // Debug.Log("Knocked back " + knockbackDistance);
+            Debug.Log("Knocked back " + knockbackDistance);
+            
             // if the obstacle knocks back or did something to all the players --> grab all the tagged players 
             // GameObject[] players;
             // players = GameObject.FindGameObjectsWithTag("Player");
